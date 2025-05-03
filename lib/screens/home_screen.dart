02a2f20a23909d6d0a3451b579d3a7374648cac0
Tobi Bamidele <bigtbamz2008@gmail.com>
2025-05-03 12:36:17 -0700

    habit_hero_1

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/background.png'),
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
